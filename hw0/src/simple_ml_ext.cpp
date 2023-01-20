d7e0c977dac82ecb41bea7c30e742a6b240d8045
Bairuic <brc730@outlook.com>
2023-01-20 16:58:12 +0800

    完成 softmax_regression_epoch_cpp

--- hw0/src/simple_ml_ext.cpp
+++ hw0/src/simple_ml_ext.cpp
@@ -6,6 +6,65 @@
 namespace py = pybind11;
 
 
+float * matrix_multip(const float * mat1, size_t rows1, size_t cols1, 
+                      float * mat2, size_t rows2, size_t cols2){
+    float *res = new float [rows1 * cols2]();
+    for(size_t i=0; i < rows1; i++)
+        for(size_t j=0; j < cols2; j++)
+            for (size_t k=0; k < cols1; k++)
+                res[i*cols2+ j] += mat1[i*cols1+k] * mat2[k*cols2+j];
+    return res;                        
+}
+
+
+float * matrix_minus(float *mat1, float *mat2, size_t rows, size_t cols){
+    size_t length = cols * rows;
+    float * res = new float [length]();
+    for (size_t i=0; i < length; i++)
+        res[i] = mat1[i] - mat2[i];
+    return res;
+}
+
+
+void softmax_normize(float *Z, size_t rows, size_t cols){
+    for (size_t i = 0; i<rows; i++){
+        double cur_row_sum = 0.0;
+
+        for (size_t j=0; j < cols; j++){
+            Z[i * cols + j] = exp(Z[i * cols + j]);
+            cur_row_sum += Z[i * cols + j];
+        }
+        
+        for (size_t j=0; j < cols; j++)
+            Z[i * cols + j] /= cur_row_sum;
+    }    
+}
+
+
+float * build_I(size_t rows, size_t cols, const unsigned char * y){
+    float * I = new float[rows * cols]();
+    for (size_t i=0; i<rows; i++)
+        I[i * cols + y[i]] = 1;
+    return I;
+}
+
+
+void update_theta(float *theta, float *dtheta, size_t rows, size_t cols, float lr, size_t batch){
+    size_t length = rows * cols;
+    for (size_t i=0; i<length; i++)
+        theta[i] -= lr / batch * dtheta[i];
+}
+
+
+float * transpose_mat(const float * X, size_t rows, size_t cols){
+    float * XT = new float[cols * rows]();
+    for(size_t i=0; i<rows; i++)
+        for(size_t j=0; j<cols; j++)
+            XT[j*rows + i] = X[i * cols + j];
+    return XT;
+}
+
+
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
 								  float *theta, size_t m, size_t n, size_t k,
 								  float lr, size_t batch)
@@ -33,7 +92,34 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      */
 
     /// BEGIN YOUR CODE
+    // forward
 
+    //const float * X_ = X;
+    //const unsigned char *y_ = y;
+    //batch = m;
+
+    for(size_t i=0; i < m; i+=batch){
+        const float * X_ = &X[i*n];
+        const unsigned char * y_ = &y[i];
+    
+        float *Z = matrix_multip(X_, batch, n, theta, n, k);
+        softmax_normize(Z, batch, k);
+        //std::cout<<"forward"<<std::endl;
+        //backward
+        float *XT = transpose_mat(X_, batch, n);
+        float *I = build_I(batch, k, y_);
+
+        float *Z_I = matrix_minus(Z, I, batch, k);
+
+        float *dtheta = matrix_multip(XT, n, batch, Z_I, batch, k);
+        update_theta(theta, dtheta, n, k, lr, batch);
+
+        delete [] Z;
+        delete [] XT;
+        delete [] Z_I;
+        delete [] I;
+        delete [] dtheta;
+    }
     /// END YOUR CODE
 }
 
