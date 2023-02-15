2f1c57b820d7d48f9c0cc0f9a1d8d9d311d8e37b
Bairuic <brc730@outlook.com>
2023-02-15 19:08:39 +0800

    Q4 完成

--- hw2/python/needle/data.py
+++ hw2/python/needle/data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .autograd import Tensor
-
+import gzip
+import struct
 from typing import Iterator, Optional, List, Sized, Union, Iterable, Any
 
 
@@ -24,7 +25,9 @@ class RandomFlipHorizontal(Transform):
         """
         flip_img = np.random.rand() < self.p
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if flip_img:
+            img = np.flip(img, axis=1)
+        return img
         ### END YOUR SOLUTION
 
 
@@ -42,7 +45,10 @@ class RandomCrop(Transform):
         """
         shift_x, shift_y = np.random.randint(low=-self.padding, high=self.padding+1, size=2)
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        padded_img = np.pad(img, ((self.padding, self.padding), (self.padding, self.padding), (0, 0)), mode='constant')
+        clipped_img = padded_img[shift_x+self.padding:shift_x+self.padding+img.shape[0],
+                                 shift_y+self.padding:shift_y+self.padding+img.shape[1]]
+        return clipped_img
         ### END YOUR SOLUTION
 
 
@@ -101,13 +107,30 @@ class DataLoader:
 
     def __iter__(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if self.shuffle:
+            temp_index = np.arange(len(self.dataset))
+            np.random.shuffle(temp_index)
+            self.ordering = np.array_split(temp_index,
+                                            range(self.batch_size, len(self.dataset), self.batch_size))
+        self.cur_batch_id = 0
         ### END YOUR SOLUTION
         return self
 
     def __next__(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if self.cur_batch_id >= len(self.ordering):
+            raise StopIteration
+        batch_data_X = [self.dataset[i][0] for i in self.ordering[self.cur_batch_id]]
+        batch_data_X = Tensor(batch_data_X)
+        if len(self.dataset[0]) == 1:
+            batch_data = (batch_data_X, )
+        else:
+            
+            batch_data_y = [self.dataset[i][1] for i in self.ordering[self.cur_batch_id]]
+            batch_data_y = Tensor(batch_data_y)
+            batch_data = (batch_data_X, batch_data_y)
+        self.cur_batch_id += 1
+        return batch_data
         ### END YOUR SOLUTION
 
 
@@ -119,17 +142,44 @@ class MNISTDataset(Dataset):
         transforms: Optional[List] = None,
     ):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.image_filename = image_filename
+        self.label_filename = label_filename
+        self.transforms = transforms
+        
+        self.images, self.labels = self._parse_mnist(self.image_filename, self.label_filename)
         ### END YOUR SOLUTION
+
+    def _parse_mnist(self, image_filename: str, label_filename: str):
+        with gzip.open(image_filename, "rb") as img_file:
+            magic_num, imgs_num, rows, cols = struct.unpack(">4I", img_file.read(16)) 
+            img_list = []
+            for _ in range(imgs_num):
+                cur_img = np.array(struct.unpack(f">{rows*cols}B", img_file.read(rows * cols)), dtype="float32")
+                img_list.append(cur_img)
+        
+        X = np.stack(img_list)
+
+        with gzip.open(label_filename, "rb") as lab_file:
+            magic_num, labs_num = struct.unpack(">2I", lab_file.read(8))
+            y = np.array(struct.unpack(f">{labs_num}B", lab_file.read(labs_num)), dtype="int8")
+        
+        X = (X - X.min()) / (X.max() - X.min())
+
+        X = X.reshape(imgs_num, rows, cols, 1)
+        return X, y
 
     def __getitem__(self, index) -> object:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        image, label = self.images[index], self.labels[index]
+        if self.transforms:
+            for transform in self.transforms:
+                image = transform(image)
+        return (image, label)
         ### END YOUR SOLUTION
 
     def __len__(self) -> int:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return self.images.shape[0]
         ### END YOUR SOLUTION
 
 class NDArrayDataset(Dataset):
